1b3134efba0b0b9256eb22fd38702baccdbe25d2
Andreas Pålsson <apals@kth.se>
2015-09-10 09:49:24 +0200

    Add std::intializer_list constructor

--- lab1/lab1/0.6_the_simple_container/UIntVector.cpp
+++ lab1/lab1/0.6_the_simple_container/UIntVector.cpp
@@ -12,11 +12,17 @@ UIntVector::UIntVector(size_t size)
 
 UIntVector::UIntVector(std::initializer_list<unsigned int> list)
 {
+    first = new unsigned int[list.size()];
+    for(int i = 0; i < list.size(); i++) {
+        //Use pointer arithmetic to dereference the elements in list
+        first[i] = *(list.begin() + i);
+    }
 }
 
 void UIntVector::reset()
 {
     for(int i = 0; i < vector_size; i++) {
+        first[i] = 0;
     }
 }
 
@@ -27,6 +33,7 @@ std::size_t UIntVector::size()
 
 int main() {
     const size_t size = 5;
+    UIntVector v1({1, 2, 3, 4, 5}); // copy list-initialization
     UIntVector v(size);
     return 0;
 }
