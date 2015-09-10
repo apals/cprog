2259c92fd1c93749b87edb50a0aba299f71d98db
oalsing <oscar.alsing@gmail.com>
2015-09-10 15:25:10 +0200

    no longer kattis compile error but wrong answer

---
+++ lab1/lab1/0.6_the_simple_container/kattis/kth_cprog_simple_container.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+#include <stdexcept>
+#include "kth_cprog_simple_container.hpp"
+
+UIntVector::UIntVector()
+{
+    first = new unsigned int;
+}
+
+UIntVector::UIntVector(size_t size)
+{
+    vector_size = size;
+    first = new unsigned int[size];
+    reset();
+}
+
+UIntVector::UIntVector(std::initializer_list<unsigned int> list)
+{
+    vector_size = list.size();
+    first = new unsigned int[list.size()];
+    for(int i = 0; i < list.size(); i++) {
+        //Use pointer arithmetic to dereference the elements in list
+        first[i] = *(list.begin() + i);
+    }
+}
+
+UIntVector::UIntVector(const UIntVector & obj) {
+    std::cout << "Copy constructor" << std::endl;
+    first = new unsigned int[obj.size()];
+    vector_size = obj.size();
+    for(int i = 0; i < obj.size(); i++) {
+        first[i] = obj[i];
+    }
+}
+
+UIntVector::UIntVector(UIntVector && obj) {
+    std::cout << "move constructor" << std::endl;
+    vector_size = obj.vector_size;
+    first = obj.first;
+
+    obj.first = nullptr;
+
+}
+
+UIntVector& UIntVector::operator=(UIntVector && other) {
+    std::cout << "move assignment operator" << std::endl;
+    vector_size = std::move(other.vector_size);
+    first = std::move(other.first);
+    return *this;
+}
+
+unsigned int& UIntVector::operator[](int index) 
+{
+    if(index < vector_size || index < 0)
+        return first[index];
+    throw std::out_of_range("Index out of range");
+}
+
+UIntVector& UIntVector::operator=(const UIntVector& other)
+{
+    UIntVector tmp(other);
+    std::cout << "jag är en dIck" << std::endl;
+    std::swap(vector_size, tmp.vector_size);
+    std::swap(first, tmp.first);
+    return *this;
+}
+
+const unsigned int& UIntVector::operator[](int index) const
+{
+    if(index < vector_size || index < 0)
+        return first[index];
+    else
+        throw std::out_of_range("Index out of range");
+}
+
+
+UIntVector::~UIntVector()
+{
+    std::cout << "destructor" << std::endl;
+    std::cout << "sizeof first: " << sizeof(first) << std::endl;
+    delete [] first;
+}
+
+void UIntVector::reset()
+{
+    for(int i = 0; i < vector_size; i++) {
+        first[i] = 0;
+    }
+}
+
+std::size_t UIntVector::size() const
+{
+    return vector_size;
+}
+/*
+int main() {
+    UIntVector a(7);
+    UIntVector b(a);
+    UIntVector c = a;
+
+    a = b;
+    a[5] = 7;
+
+    const UIntVector e(10);
+    int i = e[5];
+    i = a[0];
+    i = a[5];
+    a[5]++;
+    return 0;
+}
+*/
---
+++ lab1/lab1/0.6_the_simple_container/kattis/kth_cprog_simple_container.hpp
@@ -0,0 +1,25 @@
+#ifndef UINTVECTOR
+#define UINTVECTOR
+
+
+class UIntVector {
+
+    private:
+        size_t vector_size;
+        unsigned int * first;
+    public:
+        UIntVector();
+        UIntVector(size_t);
+        UIntVector(std::initializer_list<unsigned int>);
+        UIntVector(const UIntVector & obj);
+        UIntVector& operator =(const UIntVector&);
+        UIntVector(UIntVector&&);
+        UIntVector& operator=(UIntVector&&);
+        ~UIntVector();
+        unsigned int& operator[] (int);
+        const unsigned int& operator[] (int) const;
+        void reset();
+        std::size_t size() const;
+};
+
+#endif
