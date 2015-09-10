1e9e6d99c2d8f101da0a89c3566368bdfac84d32
Andreas Pålsson <apals@kth.se>
2015-09-10 08:46:20 +0200

    Add header file

--- lab1/lab1/0.5_will_it_float/bad_plumming.cpp
+++ lab1/lab1/0.5_will_it_float/bad_plumming.cpp
@@ -4,13 +4,15 @@ struct Data {
 
 Data ** foo(Data ** v, int x) {
     for (int i = 0; i < x; i++)
-        if (v[i] != 0)
+        //if (v[i] != 0)
             v[i] = new Data;
     return v;
 }
 
 int main () {
     const int size = 5;
-    Data ** v = new Data * [size];
+    Data ** v;
     foo(v, size);
+    delete [] v;
+    //delete [] p;
 }
---
+++ lab1/lab1/0.6_the_simple_container/UIntVector.h
@@ -0,0 +1,10 @@
+
+class UIntVector {
+
+    private:
+        size_t size;
+    public:
+        UIntVector();
+        UIntVector(size_t);
+        UIntVector(initializer_list);
+}
