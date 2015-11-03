dbb77f713bd198cbb781daba21b53f89d9a9273e
oalsing <oscar.alsing@gmail.com>
2015-11-03 14:14:33 +0100

    date header

--- lab2/Date.hpp
+++ lab2/Date.hpp
@@ -1,6 +1,24 @@
-
-
+#ifndef __DATE__
+#include <iostream>
 class Date {
+    public:
+        int year();
+        unsigned int month();
+        unsigned int day();
+        unsigned int week_day();
+        unsigned int days_per_week();
+        unsigned int days_this_month();
+        std::string week_day_name();
+        std::string month_name();
+        int add_year(int n);
+        int add_month(int n);
+        int mod_julian_day();
 
-
-}
+    private:
+        int curr_year;
+        unsigned int curr_month;
+        unsigned int curr_day;
+        unsigned int curr_week_day;
+        unsigned int curr_days_per_week;
+};
+#endif
