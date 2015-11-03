8746552499eaed62b4a400b8ab43460875631059
Andreas Pålsson <apals@kth.se>
2015-11-03 15:36:58 +0100

    fix make

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -1,6 +1,11 @@
 #include "Date.hpp"
 #include "Gregorian.hpp"
-#include "Date.hpp"
+#include <iostream>
 
 int main(){
+    Date d;
+    
+    std::cout << d.year() << std::endl;
+    std::cout << d.month() << std::endl;
+    std::cout << d.day() << std::endl;
 }
