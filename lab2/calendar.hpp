f501cd956a3714edfb90607042a1387d0a21f8d4
Andreas Pålsson <apals@kth.se>
2015-11-07 20:04:06 +0100

    start implementing calendar

--- lab2/calendar.hpp
+++ lab2/calendar.hpp
@@ -1,9 +1,16 @@
 #ifndef __LABCALENDAR__
 #define __LABCALENDAR__
+#include <string>
+#include <list>
 namespace lab2 {
+    struct Event {
+        std::string event_name;
+        int day, month, year;
+    };
     template <typename DateTime>
         class Calendar {
             public:
+                Calendar();
                 void set_date(int d, int m, int y);
 
                 bool add_event(std::string event_name, int d, int m, int y);
@@ -15,7 +22,11 @@ namespace lab2 {
                 bool remove_event(int d, int m, int y);
                 bool remove_event(int d, int m);
                 bool remove_event(int d);
-        }
+
+            private:
+                std::list<Event> integer_list;
+        };
 }
+
 #include "calendar_impl.hpp"
 #endif
