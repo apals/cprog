ecaa374307233080a13163af59d192e3b5f837a1
Andreas Pålsson <apals@kth.se>
2015-11-07 17:48:47 +0100

    return false

--- lab2/Calendar_impl.hpp
+++ lab2/Calendar_impl.hpp
@@ -1,1 +1,34 @@
 //This is the impl
+namespace lab2 {
+
+
+    void set_date(int d, int m, int y) {
+
+    }
+    bool add_event(std::string event_name, int d, int m, int y) {
+        return false;
+    }
+    bool add_event(int d, int m, int y) {
+        return false;
+    }
+    bool add_event(int d, int m) {
+        return false;
+    }
+    bool add_event(int d) {
+        return false;
+    }
+
+    bool remove_event(std::string event_name, int d, int m, int y) {
+        return false;
+    }
+    bool remove_event(int d, int m, int y) {
+        return false;
+    }
+    bool remove_event(int d, int m) {
+        return false;
+    }
+    bool remove_event(int d) {
+        return false;
+    }
+
+}
