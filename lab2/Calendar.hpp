da90741c3c4c12f053089babd7f30c97afe67c34
Andreas Pålsson <apals@kth.se>
2015-11-07 17:40:55 +0100

    add calendar

---
+++ lab2/Calendar.hpp
@@ -0,0 +1,18 @@
+
+namespace lab2 {
+    template <typename DateTime>
+        class Calendar {
+            public:
+                void set_date(int d, int m, int y);
+                
+                bool add_event(std::string event_name, int d, int m, int y);
+                bool add_event(int d, int m, int y);
+                bool add_event(int d, int m);
+                bool add_event(int d);
+
+                bool add_event(std::string event_name, int d, int m, int y);
+                bool add_event(int d, int m, int y);
+                bool add_event(int d, int m);
+                bool add_event(int d);
+        }
+}
