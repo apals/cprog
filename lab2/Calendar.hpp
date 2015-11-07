570f9c42680ddf567facd1534794f37fba3c413f
Andreas Pålsson <apals@kth.se>
2015-11-07 17:42:10 +0100

    fix

--- lab2/Calendar.hpp
+++ lab2/Calendar.hpp
@@ -4,15 +4,15 @@ namespace lab2 {
         class Calendar {
             public:
                 void set_date(int d, int m, int y);
-                
-                bool add_event(std::string event_name, int d, int m, int y);
-                bool add_event(int d, int m, int y);
-                bool add_event(int d, int m);
-                bool add_event(int d);
 
                 bool add_event(std::string event_name, int d, int m, int y);
                 bool add_event(int d, int m, int y);
                 bool add_event(int d, int m);
                 bool add_event(int d);
+
+                bool remove_event(std::string event_name, int d, int m, int y);
+                bool remove_event(int d, int m, int y);
+                bool remove_event(int d, int m);
+                bool remove_event(int d);
         }
 }
