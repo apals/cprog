e2513a13d6894c4485e0ed1c69e7aeb09d016be2
oalsing <oscar.alsing@gmail.com>
2015-11-08 00:33:09 +0100

    adds event struct constructors

--- lab2/calendar.hpp
+++ lab2/calendar.hpp
@@ -5,6 +5,11 @@
 namespace lab2 {
     struct Event {
         public: 
+            Event();
+            Event(int d) : day(d) {}
+            Event(int d, int m) : day(d), month(m) {}
+            Event(int d, int m, int y) : day(d), month(m), year(y) {}
+            Event(std::string n, int d, int m, int y) : event_name(n), day(d), month(m), year(y) {}
             std::string event_name;
             int day, month, year;
     };
