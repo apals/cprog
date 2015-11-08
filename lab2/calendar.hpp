792f477253164873dcb9ff41c91ee809efa625e1
Andreas Pålsson <apals@kth.se>
2015-11-08 12:37:52 +0100

    fix header

--- lab2/calendar.hpp
+++ lab2/calendar.hpp
@@ -21,9 +21,8 @@ namespace lab2 {
                 void set_date(int d, int m, int y);
 
                 bool add_event(std::string event_name, int d, int m, int y);
-                bool add_event(int d, int m, int y);
-                bool add_event(int d, int m);
-                bool add_event(int d);
+                bool add_event(std::string event_name, int d, int m);
+                bool add_event(std::string event_name, int d);
 
                 bool remove_event(std::string event_name, int d, int m, int y);
                 bool remove_event(int d, int m, int y);
